8b0954184ec384c91c6683fb8cd76aeb6cf04508
Jassem Al-Buloushi <jassem1988@gmail.com>
2020-03-02 09:48:07 +0300

    Initial Commit

---
+++ Local Reminders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Local Reminders
+//
+//  Created by Jassem Al-Buloushi on 3/2/20.
+//  Copyright © 2020 Jassem Al-Buloushi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
