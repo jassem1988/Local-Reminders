7d062e3f2febacf892ab87fc465dc4d88111eb75
Jassem Al-Buloushi <jassem1988@gmail.com>
2020-03-02 12:10:54 +0300

    - Create gitignore file - Embed VC in a nav controller - Import UserNotifications - Create two nav button items - Create func registerLocal to add a request permission - Create func scheduleLocal to  add content (what to show), a trigger (when to show it), and a request (the combination of content and trigger.)

--- Local Reminders/ViewController.swift
+++ Local Reminders/ViewController.swift
@@ -6,13 +6,59 @@
 //  Copyright © 2020 Jassem Al-Buloushi. All rights reserved.
 //
 
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Create nav bar buttons
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+        
+    }
+    
+    @objc func registerLocal() {
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { granted, error in
+            if granted {
+                print("YAY!")
+            } else {
+                print("OOH NO!")
+            }
+        }
+        
+    }
+    
+    @objc func scheduleLocal() {
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse eats the cheese"
+        content.categoryIdentifier = "Alarm"
+        content.userInfo = ["customData": "12345"]
+        content.sound = .default
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 10
+        dateComponents.minute = 30
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        center.add(request)
+        
+        
     }
 
 
